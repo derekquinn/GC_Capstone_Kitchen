c224ffdd3fdb13d6651f769871d6d6eb388e856c
derekquinn <quinnderek@yahoo.com>
2019-02-27 16:09:54 -0500

    initial

---
+++ src/main/java/co/kitchen/kitchen/KitchenController.java
@@ -0,0 +1,14 @@
+package co.kitchen.kitchen;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class KitchenController {
+
+	@RequestMapping("/")
+	public ModelAndView showIndex() {
+		ModelAndView mav = new ModelAndView("index");
+		return mav;
+	}
+}
