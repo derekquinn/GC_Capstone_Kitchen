9247d2b61921eeba9fd058a55e8de5544a3a5068
derekquinn <quinnderek@yahoo.com>
2019-02-27 23:17:16 -0500

    sup

--- src/main/java/co/kitchen/kitchen/RecipeService.java
+++ src/main/java/co/kitchen/kitchen/RecipeService.java
@@ -1,5 +1,22 @@
 package co.kitchen.kitchen;
 
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import co.kitchen.kitchen.model.Recipe;
+
+@Component
 public class RecipeService {
+	
+	private RestTemplate restTemplate = new RestTemplate();
+	
+	public List<Recipe> findRecipes(String search){
+		
+		String url = "https://api.edamam.com/search?q=" + search + "&app_id=8396f82d&app_key=ff9b8e6e921e07b1146fe0ba6abb4ec7";
+		return null;
+		
+	}
 
 }
