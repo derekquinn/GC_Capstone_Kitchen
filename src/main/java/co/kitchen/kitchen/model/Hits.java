4cc934a604a2c1c5dd9e72787ddd14df737e01d5
derekquinn <quinnderek@yahoo.com>
2019-02-27 21:06:05 -0500

    safety commit

--- src/main/java/co/kitchen/kitchen/model/Hits.java
+++ src/main/java/co/kitchen/kitchen/model/Hits.java
@@ -8,7 +8,33 @@ public class Hits {
 	private Boolean bookmarked;
 	private Boolean bought;
 	
-x
+	
+	public List<Recipe> getRecipe() {
+		return recipe;
+	}
+
+	public void setRecipe(List<Recipe> recipe) {
+		this.recipe = recipe;
+	}
+
+	public Boolean getBookmarked() {
+		return bookmarked;
+	}
+
+	public void setBookmarked(Boolean bookmarked) {
+		this.bookmarked = bookmarked;
+	}
+
+	public Boolean getBought() {
+		return bought;
+	}
+
+	public void setBought(Boolean bought) {
+		this.bought = bought;
+	}
+
+	
+
 		
 	}
 	
